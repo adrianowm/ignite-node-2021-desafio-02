5e3b33af001744cde4da7d53a99baa01efb5200b
Adriano Machado <adriano.willians@gmail.com>
2023-10-10 08:22:37 -0300

    chapter 01 - desafio 02 - done

--- src/index.js
+++ src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404)
+  }
+
+  request.user = user
+
+  next()
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if(!user.pro && user.todos.length >= 10) {
+    return response.status(403)
+  }
+
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  if(!validate(id)){
+    return response.status(400)
+  }
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404)
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo){
+    return response.status(404)
+  }
+
+  request.user = user
+  request.todo = todo
+
+  next()
+  
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if(!user){
+    return response.status(404)
+  }
+
+  request.user = user
+
+  next()
 }
 
 app.post('/users', (request, response) => {
